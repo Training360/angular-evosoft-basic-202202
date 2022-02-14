67435dcf6bfc6fe2f1290621ccd1c145364f7549
Joe Cserko <j.cserko@gmail.com>
2022-02-14 16:17:51 +0100

    components' interaction and data-bindings

--- angular-components/src/app/app.component.ts
+++ angular-components/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-components';
+  myTitle = 'Evosoft Course';
+  subTitle: string = '';
+  numberOfClicks: number = 0;
+  buttonClass: {[key: string]: boolean} = {
+    'btn-primary': true,
+    'btn-secondary': false,
+    'btn-success': false,
+    'btn-info': false,
+  };
+
+  showGreeting(): void {
+    this.numberOfClicks++;
+
+    const keys: string[] = Object.keys(this.buttonClass);
+    const randIndex: number = Math.floor(
+      Math.random() * keys.length
+    );
+    const currentKey: string = keys[randIndex];
+    for (const key of keys) {
+      this.buttonClass[key] = currentKey === key;
+    }
+
+    this.subTitle = `${this.myTitle}:${this.numberOfClicks}`;
+  }
 }
--- angular-components/src/app/app.module.ts
+++ angular-components/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
